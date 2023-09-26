e61a1d1654fefd198d2b8390e69d5da6fb63e1c5
aasmah <aas.29980@gmail.com>
2023-09-25 21:18:45 -0500

    ver upgrade

---
+++ my-project/config/api/order/controllers/Order.js
@@ -0,0 +1,56 @@
+'use strict';
+/**
+ * Order.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Order`.
+ */
+// note that this needs to be a "private" key from STRIPE
+ const stripe = require('stripe')(process.env.STRIPE_PK);
+
+/**
+ * Given a dollar amount number, convert it to it's value in cents
+ * @param number 
+ */
+const fromDecimalToInt = (number) => parseInt(number * 100)
+
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  /**
+   * Create a/an order record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    const { address, amount, dishes, token, city, state } = JSON.parse(
+      ctx.request.body
+    );
+    const stripeAmount = Math.floor(amount * 100);
+    // charge on stripe
+    const charge = await stripe.charges.create({
+      // Transform cents to dollars.
+      amount: stripeAmount,
+      currency: 'usd',
+      description: `Order ${new Date()} by ${ctx.state.user._id}`,
+      source: token,
+    });
+
+    // Register the order in the database
+    const order = await strapi.services.order.create({
+      user: ctx.state.user.id,
+      charge_id: charge.id,
+      amount: stripeAmount,
+      address,
+      dishes,
+      city,
+      state,
+    });
+
+    return order;
+  },
+};
